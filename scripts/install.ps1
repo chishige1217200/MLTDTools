dad613792ea3b072ba88aeca8e53340b1b71cec4
chishige1217200 <daichicchi01@gmail.com>
2025-03-08 16:26:24 +0900

    Replace Start-FileDownload with Invoke-WebRequest for improved file download

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,2 +1,2 @@
-Start-FileDownload "https://damassets.autodesk.net/content/dam/autodesk/www/adn/fbx/2020-0-1/fbx202001_fbxsdk_vs2017_win.exe" "fbxsdk.exe"
+Invoke-WebRequest -Uri "https://damassets.autodesk.net/content/dam/autodesk/www/adn/fbx/2020-0-1/fbx202001_fbxsdk_vs2017_win.exe" -OutFile "fbxsdk.exe"
 Start-Process -FilePath "fbxsdk.exe" /S -Wait
